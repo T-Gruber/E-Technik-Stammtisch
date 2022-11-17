f02f2fc7838b4a6a92b995515a4480a6a77eda0a
Tobias Gruber <tobiasgruber@b173.hm.vpn.lrz.de>
2022-11-17 08:18:34 +0100

    Update

--- statistics.py
+++ statistics.py
@@ -1,12 +1,15 @@
 
 
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 def read_csv(path_to_csv):
 
     with open(path_to_csv, "r") as csv_file:
         lines = csv_file.readlines()
+        raw_content = []
+        for line in lines:
+            raw_content.append([l.strip() for l in line.split(";")])
         header = lines[0].split(";")
         content = []
         for line in lines[1:]:
@@ -16,7 +19,7 @@ def read_csv(path_to_csv):
                 cell_dict[header[i].strip()] = cells[i].strip()
             content.append(cell_dict)                
 
-    return content
+    return content, raw_content
 
 
 def get_stats(content):
@@ -38,12 +41,31 @@ def plot_stats(count_dict, mode = "plot"):
         plt.show()
     elif mode == "save":
         plt.savefig('statistics.png')
+    plt.cla()
+
+def plot_map(content, mode):
+
+    names = [n for n in content[0][2:]]
+    dates = [d[0] for d in content[1:]]
+    values = []
+    for c in content[1:]:
+        values.append([1 if i=="x" else 0 for i in c[2:]])
+    plt.imshow( values , cmap = 'RdYlGn')
+    plt.xticks(range(len(names)), names)
+    plt.yticks(range(len(dates)), dates)
+    if mode == "plot":
+        plt.show()
+    elif mode == "save":
+        plt.savefig('map.png')
+    plt.cla()
 
 
 if __name__ == "__main__":
 
-    content     = read_csv("2022_Auswertung.csv")
+    content, raw_content = read_csv("2022_Auswertung.csv")
     count_dict  = get_stats(content)
 
-    plot_stats(count_dict, "plot")
+    #plot_stats(count_dict, "plot")
     plot_stats(count_dict, "save")
+    #plot_map(raw_content, "plot")
+    plot_map(raw_content, "save")
